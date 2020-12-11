2491cf85e670f630153f760fa83c81fc5bd039ab
Katherine Hébert <katherine.ahebert@gmail.com>
2020-12-11 13:55:49 -0500

    specify mapselector pkg

--- R/app_server.R
+++ R/app_server.R
@@ -5,7 +5,9 @@
 #' @import shiny
 #' @noRd
 app_server <- function( input, output, session ) {
+  
   # Your application server logic 
-  output$map <- leaflet::renderLeaflet(make_leaflet_map())
+  output$map <- leaflet::renderLeaflet(mapselector::make_leaflet_map())
   #mod_modal_make_server("modal_make_ui_1", region = reactive(input$map_shape_click$id))
+  
 }
