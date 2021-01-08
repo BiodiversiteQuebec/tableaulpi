57e3bf47ffa9d8e6512587a150443f65c7c6dd5c
Guillaume Larocque <glaroc@gmail.com>
2021-01-08 11:34:36 -0500

    Change look and fix left menu

--- inst/app/www/tableau.js
+++ inst/app/www/tableau.js
@@ -1,17 +1,21 @@
 (function($) {
 $(document).ready(function(){
+  var openclosed="open"
+  $("#closebtn").html('&#9664;');
   $('#closebtn').on('click',function(){
-    divs=$("#main-row").children();
-    if($(divs[0]).css('margin-left') == "-22vw"){
-      $(divs[0]).css('margin-left',0);
-      $(divs[1]).css('width',"80vw");
+    var divs=$("#main-row").children();
+    if(openclosed=="closed"){
+      openclosed="open"
+      $(divs[0]).animate({'margin-left':0});
+      $(divs[1]).animate({'width':"80vw"});
       $("#map").width("80vw");
-      $("#closebtn").html('<');
+      $("#closebtn").html('&#9664;');
     }else{
-      $(divs[0]).css('margin-left',"-22vw");
-      $(divs[1]).css('width',"96vw");
+      openclosed="closed"
+      $(divs[0]).animate({'margin-left':"-23vw"});
+      $(divs[1]).animate({'width':"96vw"});
       $("#map").width("96vw");
-      $("#closebtn").html('>');
+      $("#closebtn").html('&#9654;');
     }
   });
 });
