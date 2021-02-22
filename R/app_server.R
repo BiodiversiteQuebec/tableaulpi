7e2ef76b11e32452c5d52cfa525385727c470242
Katherine Hébert <katherine.ahebert@gmail.com>
2021-02-22 15:10:16 -0500

    put population bubble plot in a modal

--- R/app_server.R
+++ R/app_server.R
@@ -3,6 +3,7 @@
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @import mapselector
 #' @noRd
 app_server <- function( input, output, session ) {
 
@@ -16,8 +17,8 @@ app_server <- function( input, output, session ) {
                                          title_text = "Categories d'espece",
                                          md_file = "second_modal_text.md")
 
+  # LPI time series plot
   mod_lpi_time_series_server("lpi", taxachoice =  taxachoice)
-
   mapselector::mod_modal_observeEvent_server("affiche_index",
                                              title_format_pattern =  "Indice Planète Vivante pour %s",
                                              title_var = taxachoice,
@@ -27,7 +28,16 @@ app_server <- function( input, output, session ) {
                                              ), type = "hidden")
 
   # "Tendance par population
-  output$poptrend <- plotly::renderPlotly(make_poptrend(taxa = taxachoice()))
+  #output$poptrend <- plotly::renderPlotly(make_poptrend(taxa = taxachoice()))
+  mod_population_bubbleplot_server("population_bubbleplot_ui_1", taxachoice = taxachoice)
+  mapselector::mod_modal_observeEvent_server("affiche_poptrend",
+                                             title_format_pattern =  "Taux de croissance des populations pour %s",
+                                             title_var = taxachoice,
+                                             tabPanel(
+                                               title = "title",
+                                               mod_population_bubbleplot_ui("population_bubbleplot_ui_1")
+                                             ), type = "hidden")
+  
 
   # "À propos de l'indice"
   output$about <- renderUI({includeHTML("data/apropos_lpi.html")})
