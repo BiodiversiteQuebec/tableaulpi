4d043ae4f71e61dc87058a954e47a36c0c6339c1
Katherine Hébert <katherine.ahebert@gmail.com>
2021-03-10 15:40:57 -0500

    add spacing to improve readability

--- R/app_ui.R
+++ R/app_ui.R
@@ -6,11 +6,21 @@
 #' @import mapselector
 #' @noRd
 app_ui <- function(request) {
+  
   tagList(
+    
+    # information panel that slides onto window (appears on hover)
     mapselector::marcel(filename = "firstModal.md"),
+    
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # Your application UI logic 
+    
+    
+    # Application UI logic -----------------------------------------------------
+    
+    
+    # Standard mapselector dashboard format ----
+    
     mapselector::tableau_de_bord(
       mapselector::dash_title(title = "Indice Planète Vivante"), 
       mapselector::dash_sidebar(
@@ -20,15 +30,36 @@ app_ui <- function(request) {
           mapselector::mod_modal_helpbutton_ui(id = "spp_help",
                                                "Choisir le groupe d'espèces")
         ),
-        mapselector::mod_modal_observeEvent_ui("affiche_index", button_text = "Afficher l'IPV"),
+        
+        
+        
+        # Modals that appear when a button is clicked ----
+        
+        # Index trend through time
+        mapselector::mod_modal_observeEvent_ui("affiche_index", button_text = "Afficher l'IPV"), 
+        
+        # Density plot showing the distributions of index values per group 
         mapselector::mod_modal_observeEvent_ui("affiche_ridgeplot", button_text = "IPV par groupe"),
+        
+        # Point plot showing individual populations' trends
         mapselector::mod_modal_observeEvent_ui("affiche_poptrend", button_text = "IPV par population"),
+        
+        # Pop-up information box
         mapselector::mod_modal_observeEvent_ui("affiche_tuto", button_text = "Guide d'utilisation")
       ),
+      
+      
+      # Tabs that appear on the main dashboard ----
       mapselector::dash_tabs(
+        
+        
+        # First tab: Map on landing page
         #maybe a little strange, but here we pass in the UI of a modal and the id that defines it.
         mapselector::tab_map(title = "Carte", id = "pointmap",
                              outputFunction = mapselector::mod_map_select_ui),
+        
+        
+        # Second tab: more detailed information about the index
         mapselector::tab_gen(title = "À propos de l'indice", 
                 outputFunction = htmlOutput, 
                 id = "about")
