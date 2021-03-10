af50f1bb7acc26c8b12671a294eb1066a2166676
Katherine Hébert <katherine.ahebert@gmail.com>
2021-03-10 16:49:52 -0500

    make it prettier and add some information

--- R/utils_timeseries.R
+++ R/utils_timeseries.R
@@ -4,12 +4,30 @@
 
 make_timeseries <- function(clicked_population){
 
+  # add palette from the map here (for now... this is to improve) ----
+  pal <- c("amphibiens" = "#56B4E9",
+           "mammifères" = "#D55E00", 
+           "oiseaux" = "#E69F00", 
+           "poissons" = "#0072B2", 
+           "reptiles" = "#009E73",
+           "#999999") # will need invertebrate colour here when survol is added ----
+  
+  # import dataset
   df <- readRDS("data/lpd_qc_fake.RDS")
   df <- df[which(df$scientific_name == clicked_population),]
+  group_name <- df$taxa[1]
   
-  ggplot(df, aes(x = year_obs, y = obs_value)) +
-    geom_line() +
+  # plot raw time series
+  ggplot(df, 
+         aes(x = year_obs, 
+                 y = obs_value)) +
+    geom_line(col = "#4d4d4d") +
+    geom_point(col = pal[group_name], 
+               size = 4) +
+    scale_color_manual(values = pal) +  # use palette from above
     labs(x = "",
-         y = "Abondance de la population") +
-    theme_classic()
+         y = "Abondance de la population",
+         caption = paste0("Source des données: ", 
+                          unique(df$intellectual_rights))) +
+    theme_mapselector()
 }
