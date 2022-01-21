1be373c16aaba9de90cc6e61b906a2901030d341
Katherine Hébert <katherine.ahebert@gmail.com>
2022-01-21 10:06:03 -0500

    Update with ratlas data

--- R/utils_timeseries.R
+++ R/utils_timeseries.R
@@ -12,34 +12,47 @@
 
 make_timeseries <- function(clicked_population){
 
-  # add palette from the map here (for now... this is to improve) ----
-  pal <- c("amphibiens" = "#56B4E9",
-           "mammifères" = "#D55E00", 
-           "oiseaux" = "#E69F00", 
-           "poissons" = "#0072B2", 
-           "reptiles" = "#009E73",
+  ## colour palette ##
+  pal <- c("Amphibiens" = "#56B4E9",
+           "Mammifères" = "#D55E00", 
+           "Oiseaux" = "#E69F00", 
+           "Poissons" = "#0072B2", 
+           "Reptiles" = "#009E73",
            "#999999")
   
   # import dataset
-  df <- readRDS("data/lpd_qc_fake.RDS")
-  df <- df[which(df$org_event == clicked_population),]
-  group_name <- df$taxa[1]
+  # set api token as environment variable
+  Sys.setenv("ATLAS_API_TOKEN" = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoicmVhZF9vbmx5X2FsbCIsIm1haWwiOiJrYXRoZXJpbmUuaGViZXJ0QHVzaGVyYnJvb2tlLmNhIn0.jHfCLsRseU0--5qFB5A_PfIOEv0I24PQw1ip3q_3KQw')
+  
+  # get time series and taxonomic info to observations
+  obs <- dplyr::left_join(ratlas::get_timeseries(), 
+                          ratlas::get_gen(endpoint="taxa"), 
+                          by = c("id_taxa" = "id"))
+  obs$id <- as.character(obs$id)
+  
+  df <- obs[which(obs$id == clicked_population),]
+  
+  df_plot <- data.frame(
+    "years" = unlist(df$years), 
+    "values" = unlist(df$values)
+    )
   
   # plot raw time series
-  ggplot(df, 
-         aes(x = year_obs, 
-                 y = obs_value)) +
+  ggplot(data = df_plot,
+         aes(x = years, y = values)) +
     geom_line(col = "#4d4d4d") +
-    geom_point(col = pal[group_name], 
+    geom_point(col = pal[df$species_gr[1]], 
                size = 4) +
     scale_color_manual(values = pal) +  # use palette from above
     scale_x_continuous(breaks = function(x) seq(ceiling(x[1]), floor(x[2]), by = 1)) + # round to integer, since the x axis is years
     labs(x = "",
          y = "Abondance de la population",
          title = paste0(
-           gsub("_", " ", df$scientific_name[1]), 
-           " (", df$common_name[1], ")"),
-         caption = paste0("Source des données: ", 
-                          unique(df$intellectual_rights))) +
+           gsub("_", " ", df$scientific_name[1])#, 
+           #" (", df$common_name[1], ")"
+           )#,
+         # caption = paste0("Source des données: ", 
+         #                  unique(df$intellectual_rights))
+         ) +
     theme_mapselector()
 }
